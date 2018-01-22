c025e62dc877e741f0b3dd646902f284031183fe
puneet-s <35294511+puneet-s@users.noreply.github.com>
2018-01-23 00:03:35 +0530

    Add files via upload

---
+++ Build_DTM_DtmIdf.R
@@ -0,0 +1,31 @@
+Build_dtm_dtmidf = function (clean.text)
+  
+{
+  
+  library(dplyr)
+  library(tidytext)
+  require(topicmodels)
+  
+  
+  x = clean.text
+  text_df <- data_frame(line = 1:length(x), text = x)
+  
+  
+  text_words <- text_df %>%
+    unnest_tokens(word, text) %>%
+    count(line, word) 
+  
+  text_words_idf <- text_words %>%
+    bind_tf_idf(word, line, n)
+  
+  
+  dtm <-text_words%>%
+    cast_dtm(line, word, n)
+  
+  dtm_idf <- text_words_idf %>%
+    cast_dtm(word, line, n)
+  
+  out = list (dtm, dtm_idf)
+  return (out)
+  
+}
